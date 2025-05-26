993f2ab258a2022b1773cf3adcdf0ff2167e1196
admin <haic1947@gmail.com>
2025-05-27 02:33:23 +0700

    refactor: admin

---
+++ WebsiteBanHang/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteBanHang.Models;
+using WebsiteBanHang.Repositories;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public AdminController(IProductRepository productRepository, ICategoryRepository categoryRepository)
+        {
+            _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var totalProducts = (await _productRepository.GetAllAsync()).Count();
+            var totalCategories = (await _categoryRepository.GetAllAsync()).Count();
+
+            ViewBag.TotalProducts = totalProducts;
+            ViewBag.TotalCategories = totalCategories;
+
+            return View();
+        }
+    }
+}
---
+++ WebsiteBanHang/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteBanHang.Models;
+using WebsiteBanHang.Repositories;
+
+namespace WebsiteBanHang.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IProductRepository _productRepository;
+
+        public CategoryController(ICategoryRepository categoryRepository, IProductRepository productRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _productRepository = productRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryRepository.GetAllAsync();
+            return View(categories);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryRepository.AddAsync(category);
+                TempData["SuccessMessage"] = "Thêm danh mục thành công!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _categoryRepository.UpdateAsync(category);
+                TempData["SuccessMessage"] = "Cập nhật danh mục thành công!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var category = await _categoryRepository.GetByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            if (await CategoryHasProducts(id))
+            {
+                ViewBag.ErrorMessage = "Không thể xóa danh mục này vì có sản phẩm đang thuộc về danh mục.";
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("DeleteConfirmed")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (await CategoryHasProducts(id))
+            {
+                TempData["ErrorMessage"] = "Không thể xóa danh mục này vì có sản phẩm đang thuộc về danh mục.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            await _categoryRepository.DeleteAsync(id);
+            TempData["SuccessMessage"] = "Xóa danh mục thành công!";
+            return RedirectToAction(nameof(Index));
+        }
+
+        private async Task<bool> CategoryHasProducts(int categoryId)
+        {
+            var products = await _productRepository.GetAllAsync();
+            return products.Any(p => p.CategoryId == categoryId);
+        }
+    }
+}
