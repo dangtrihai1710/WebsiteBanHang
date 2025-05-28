768c4cf9ec4d348bcec1dc45ee49d5123fc25ba5
admin <haic1947@gmail.com>
2025-05-28 19:08:18 +0700

    fix: cart

--- WebsiteBanHang/Models/ApplicationDbContext.cs
+++ WebsiteBanHang/Models/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿// Models/ApplicationDbContext.cs - Cập nhật để bao gồm Order và OrderDetail
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebsiteBanHang.Models
 {
-    // Thay đổi từ IdentityDbContext thành IdentityDbContext<ApplicationUser>
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
@@ -13,6 +13,8 @@ namespace WebsiteBanHang.Models
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,6 +23,14 @@ namespace WebsiteBanHang.Models
             // Cấu hình decimal precision cho Price
             modelBuilder.Entity<Product>()
                 .Property(p => p.Price)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<Order>()
+                .Property(o => o.TotalPrice)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<OrderDetail>()
+                .Property(od => od.Price)
                 .HasPrecision(18, 2);
 
             // Product relationships
@@ -34,6 +44,23 @@ namespace WebsiteBanHang.Models
                 .HasOne(pi => pi.Product)
                 .WithMany(p => p.Images)
                 .HasForeignKey(pi => pi.ProductId);
+
+            // Order relationships
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.ApplicationUser)
+                .WithMany()
+                .HasForeignKey(o => o.UserId);
+
+            // OrderDetail relationships
+            modelBuilder.Entity<OrderDetail>()
+                .HasOne(od => od.Order)
+                .WithMany(o => o.OrderDetails)
+                .HasForeignKey(od => od.OrderId);
+
+            modelBuilder.Entity<OrderDetail>()
+                .HasOne(od => od.Product)
+                .WithMany()
+                .HasForeignKey(od => od.ProductId);
         }
     }
 }
